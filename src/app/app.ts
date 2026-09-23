218194f4e5e595dcda9cd8352665a1f6190a4df1
vishwa-bhajantri <vishwabhajantri98@gmail.com>
2026-09-23 19:59:03 +0530

    Angular Practice Session

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
+
 export class App {
-  protected readonly title = signal('my-angular-app');
+
+        //String Interpolation 
+        ProjectName = "Angular";
+        title = "Basic App";
 }
