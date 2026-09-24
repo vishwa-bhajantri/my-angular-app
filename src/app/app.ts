085c7fbd55b535fde6b4c3435a5382cd0fcd0c80
vishwa-bhajantri <vishwabhajantri98@gmail.com>
2026-09-24 08:49:23 +0530

    Angular Practice Session

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,49 @@ import { Component} from '@angular/core';
 
 export class App {
 
+  // constructor(){
+  //   setTimeout(() => {
+  //     this.customInputType = "checkbox";
+  //   }, 50);
+  // }
+
         //String Interpolation 
-        ProjectName = "Angular";
-        title = "Basic App";
+        ProjectName: string = "Angular";
+        title: string = "Basic App";
+
+        getCustomerName(): string{
+          return "Steve"
+        }
+
+        //property Data Binding 
+
+        customerRole: string = "Admin";
+
+        Message:string = "Login Hours 10 am to 11 am";
+
+        // customInputType = "checkbox"
+
+        customInputType:string = "radio"
+
+        //event binding
+
+        btnClick(){
+          console.log("Btn Clicked");
+          console.log(this.EmpName);
+        }
+
+        EmpName = ""
+
+        placeHolderMessage = "please enter your name";
+        captureData(eventDetails: KeyboardEvent){
+          //console.log("you enter some charecter");
+          
+          var inpE1 = eventDetails.target as HTMLInputElement;
+          // console.log(inpE1);
+          // console.dir(inpE1);
+
+          console.log(inpE1.value);
+          this.EmpName = inpE1.value;
+        }
+
 }
